6b53922524c0641e2c09df6dc8657132937883b1
Guillermo-Buzzoni <83556848+Guillermo-Buzzoni@users.noreply.github.com>
2026-01-19 22:31:22 +0000

    updated scripts file

--- pipeline/run scripts.sh
+++ pipeline/run scripts.sh
@@ -33,8 +33,6 @@ docker run -it --rm \
     --pg-port=5432 \
     --pg-db=ny_taxi \
     --target-table=yellow_taxi_trips_2021_2 \
-    --year=2021 \
-    --month=2 \
     --chunksize=100000
 
 # In another terminal, run pgAdmin on the same network
